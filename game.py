de00b874134b1663b6f935a2b18dbef17702a9a1
LeeVaughn <dlvprogramming@gmail.com>
2019-08-24 10:50:12 -0700

    added random word functionality

--- game.py
+++ game.py
@@ -1,3 +1,5 @@
+import random
+
 def hangman(word):
   wrong = 0
   stages = ["",
@@ -6,12 +8,14 @@ def hangman(word):
             "|      0      ",
             "|     /|\     ",
             "|     / \     ",
-            "|"]
+            ""]
   rletters = list(word)
-  board = ["_"] * len(word)
+  board = ["__"] * len(word)
   win = False
 
   print("Welcome to Hangman!")
+  print("The word is a food that Lee likes...")
+  print(" ".join(board))
 
   # continues the game until wrong is no longer less than the length of stages - 1
   while wrong < len(stages) - 1:
@@ -38,7 +42,7 @@ def hangman(word):
     # uses e to slice up to the current stage of the game
     print("\n".join(stages[0: e]))
 
-    if "_" not in board:
+    if "__" not in board:
       print("You win!")
       print(" ".join(board))
 
@@ -49,34 +53,5 @@ def hangman(word):
     print("\n".join(stages[0: wrong]))
     print("You lose grandpa! It was {}.".format(word))
 
-hangman("deadwood")
-    
-
-# def hangman(word):
-#     wrong_guesses = 0
-#     stages = ["", "________      ", "|      |      ", "|      0      ", "|     /|\     ", "|     / \     ", "|"]
-#     remaining_letters = list(word)
-#     letter_board = ["__"] * len(word)
-#     win = False
-#     print("Welcome to Hangman")
-#     while wrong_guesses < len(stages) - 1:
-#         print('\n')
-#         guess = input("Guess a letter")
-#         if guess in remaining_letters:
-#             character_index = remaining_letters.index(guess)
-#             letter_board[character_index] = guess
-#             remaining_letters[character_index] = '$'
-#         else:
-#             wrong_guesses += 1
-#         print((' '.join(letter_board)))
-#         print('\n'.join(stages[0: wrong_guesses + 1]))
-#         if '__' not in letter_board:
-#             print('You win! The word was:')
-#             print(' '.join(letter_board))
-#             win = True
-#             break
-#     if not win:
-#         print('\n'.join(stages[0: wrong_guesses]))
-#         print('You lose! The words was {}'.format(word))
-
-# hangman("cat")
+words = ["pizza", "chinese", "tacos", "pistachios", "burgers", "pockets"]
+hangman(random.choice(words))
